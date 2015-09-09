3ab8dfaead54b39f17d55ec48439da6c45c27e2a
ajfaraday <andrewfaraday@hotmail.co.uk>
2015-09-09 23:00:45 +0100

    moved to generic quad behaviour (gateway to corners)

--- lib/road.rb
+++ lib/road.rb
@@ -1,33 +1,39 @@
- class Road
+class Road
 
   include RoadComponents::Corners
 
   COLOUR = Gosu::Color::GRAY
   WIDTH = 40
 
-  attr_accessor :x1, :y1, :X2, :Y2
+  attr_accessor :x1, :y1, :X2, :y2
+  attr_accessor :quads
 
-  def initialize(x1=100,y1=100,x2=100,y2=300)
+  def initialize(x1=100, y1=100, x2=100, y2=300)
     @x1, @y1, @x2, @y2 = x1, y1, x2, y2
     @angle = Gosu.angle(@x1, @y1, @x2, @y2)
-    puts @angle
+    @quads = []
+    @quads << [
+      front_right, front_left,
+      back_right, back_left
+    ]
   end
 
   def draw
-    Gosu.draw_quad(
-      front_left[0], front_left[1], Road::COLOUR,
-      front_right[0], front_right[1], Road::COLOUR,
-      back_left[0], back_left[1], Road::COLOUR,
-      back_right[0], back_right[1], Road::COLOUR,
-      Layer[:road]
-    )
+    quads.each do |quad|
+      Gosu.draw_quad(
+        quad[0][0], quad[0][1], Road::COLOUR,
+        quad[1][0], quad[1][1], Road::COLOUR,
+        quad[2][0], quad[2][1], Road::COLOUR,
+        quad[3][0], quad[3][1], Road::COLOUR,
+        Layer[:road]
+      )
+    end
 
-    # TODO remove once debugged
     Gosu.draw_line(
       @x1, @y1, Gosu::Color::WHITE,
       @x2, @y2, Gosu::Color::WHITE,
       Layer[:road_paint]
     )
   end
-  
+
 end
